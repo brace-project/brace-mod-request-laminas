43da80b89360a44c2824a761219d25b5ae66d532
Matthes <matthes@leuffen.de>
2021-11-02 12:25:52 +0100

    added empty result

--- src/LaminasResponseFactoryBridge.php
+++ src/LaminasResponseFactoryBridge.php
@@ -5,6 +5,7 @@ namespace Brace\Mod\Request\Zend;
 
 
 use Brace\Core\BraceResponseFactoryInterface;
+use Laminas\Diactoros\Response\EmptyResponse;
 use Laminas\Diactoros\Response\TextResponse;
 use Laminas\Diactoros\ResponseFactory;
 use Psr\Http\Message\ResponseInterface;
@@ -15,5 +16,10 @@ class LaminasResponseFactoryBridge extends ResponseFactory implements BraceRespo
     public function createResponseWithBody(string $body, int $code = 200, array $headers=[]): ResponseInterface
     {
         return new TextResponse($body, $code, $headers = []);
+    }
+
+    public function createResponseWithoutBody(int $code = 200, array $headers=[]): ResponseInterface
+    {
+        return new EmptyResponse($code, $headers);
     }
 }
