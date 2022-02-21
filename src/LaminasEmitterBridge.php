2617b000e6390f14202329943f58c4355173077a
Matthes <matthes@leuffen.de>
2022-02-21 15:52:24 +0100

    update

--- src/LaminasEmitterBridge.php
+++ src/LaminasEmitterBridge.php
@@ -8,6 +8,7 @@ use Brace\Core\EmitterInferface;
 use Laminas\HttpHandlerRunner\Emitter\EmitterStack;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Laminas\HttpHandlerRunner\Emitter\SapiStreamEmitter;
+use Laminas\HttpHandlerRunner\Exception\EmitterException;
 use Psr\Http\Message\ResponseInterface;
 
 class LaminasEmitterBridge implements EmitterInferface
@@ -18,6 +19,13 @@ class LaminasEmitterBridge implements EmitterInferface
         $stack = new EmitterStack();
         $stack->push(new SapiEmitter());
         $stack->push(new SapiStreamEmitter());
-        $stack->emit($response);
+        try {
+            $stack->emit($response);
+        } catch (EmitterException $e) {
+            if (headers_sent($file, $line)) {
+                throw new \Error($e->getMessage() . " (Output stated in: '$file' on Line: $line)", 0, $e);
+            }
+            throw new EmitterException($e->getMessage() . " (turn output buffering off to see the file and line - use ob_end_flush() to disable)", $e->getCode(), $e);
+        }
     }
 }
