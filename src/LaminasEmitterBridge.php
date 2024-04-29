798b650166d29a5b433d89be7362f9ff6393fd96
Matthias Leuffen <matthes@leuffen.de>
2024-04-29 16:08:09 +0200

    turned off output compression and implicit flush on streamed responses

--- src/LaminasEmitterBridge.php
+++ src/LaminasEmitterBridge.php
@@ -21,6 +21,10 @@ class LaminasEmitterBridge implements EmitterInferface
         $stack->push(new SapiEmitter());
         $stack->push(new StreamedResponseEmitter());
         $stack->push(new SapiStreamEmitter());
+        @ini_set('zlib.output_compression',0); // Turn off compression (won't work with this)
+        @ini_set('implicit_flush',1);
+        @ob_end_clean();
+        
         try {
             $stack->emit($response);
         } catch (EmitterException $e) {
